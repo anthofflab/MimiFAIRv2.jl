9cf149589813ff5b6f9b5272c2548371507e9d74
lrennels <lrennels@berkeley.edu>
2021-07-19 16:08:46 -0700

    fix paths

--- src/monte_carlo.jl
+++ src/monte_carlo.jl
@@ -27,10 +27,10 @@ function create_fair_monte_carlo(n_samples::Int;
                                 )
 
     #Load constrained FAIR parameters from Leach et al. (2021).
-    thermal_params           = DataFrame(load(joinpath(@__DIR__, "..", "data", "constrained_parameters", "julia_constrained_thermal_parameters_average_probs.csv")))
-    gas_params               = DataFrame(load(joinpath(@__DIR__, "..", "data", "constrained_parameters", "julia_constrained_gas_cycle_parameters_average_probs.csv")))
-    indirect_forcing_params  = DataFrame(load(joinpath(@__DIR__, "..", "data", "constrained_parameters", "julia_constrained_indirect_forcing_parameters_average_probs.csv")))
-    exogenous_forcing_params = DataFrame(load(joinpath(@__DIR__, "..", "data", "constrained_parameters", "julia_constrained_exogenous_forcing_parameters_average_probs.csv")))
+    thermal_params           = DataFrame(load(joinpath(data_dir, "julia_constrained_thermal_parameters_average_probs.csv")))
+    gas_params               = DataFrame(load(joinpath(data_dir, "constrained_parameters", "julia_constrained_gas_cycle_parameters_average_probs.csv")))
+    indirect_forcing_params  = DataFrame(load(joinpath(data_dir, "data", "constrained_parameters", "julia_constrained_indirect_forcing_parameters_average_probs.csv")))
+    exogenous_forcing_params = DataFrame(load(joinpath(data_dir, "data", "constrained_parameters", "julia_constrained_exogenous_forcing_parameters_average_probs.csv")))
 
     # Create random indices and create a subset of FAIR sample id values (for indexing data).
     rand_indices     = sort(sample(1:93995, n_samples, replace=false))
